5bb9a76b80c2f5cb99a734f9c2d0125b667e8b4b
Giorgio Borelli <casaborelli@gmail.com>
2013-05-30 18:10:09 +0200

    moved GEO_STYLE_FIELDS from c.geo.contentlocations

--- collective/geo/settings/config.py
+++ collective/geo/settings/config.py
@@ -1,1 +1,14 @@
 PROJECTNAME = "collective.geo.settings"
+
+GEO_STYLE_FIELDS = [
+    'use_custom_styles',
+    'marker_image',
+    'map_viewlet_position',
+    'marker_image_size',
+    'map_width',
+    'linecolor',
+    'map_height',
+    'polygoncolor',
+    'display_properties',
+    'linewidth',
+]
